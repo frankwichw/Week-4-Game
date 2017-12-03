9f33c6770501ba8c1b26cd5baf4c9d503d61333a
James Frankwich <jamesfrankwich@Jamess-MacBook-Pro.local>
2017-12-03 17:25:23 -0500

    Initial site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,75 @@
 $(document).ready(function(){
-	// generate random image of your ghost
-	randomImg();
-	// generate random number for your ghost's weight
-	generateGhostWeight();
-});
+	var wins = 0;
+	$("#winsHere").text(wins);
+	var losses = 0;
+	$("#lossesHere").text(losses);
 
-function randomImg(){
+	var score = 0;
+	$("#theirWeight").text(score);
+
+	var ghostOneWeight = 0;
+	var ghostTwoWeight = 0;
+	var ghostThreeWeight = 0;
+	var ghostFourWeight = 0;
+	// generate random image of your ghost
 	var randomNumber = Math.floor(Math.random()*(4-1+1)+1);
 	var imgName = "yourghost" + randomNumber + ".png";
 	document.getElementById("imageid").src= "assets/images/" + imgName;
-};
 
-function generateGhostWeight(){
+	// generate random number for your ghost's weight
 	var randomGhostNumber = Math.floor(Math.random()*(120-1+19)+1);
 	var ghostWeight = randomGhostNumber
+	$("#weight").text(ghostWeight);
+
+	$(".firstGhost").click(function(){
+		var randomNumber = Math.floor(Math.random()*(12-1+1)+1);
+		var ghostOneWeight = randomNumber;
+		score += ghostOneWeight;
+		$("#theirWeight").text(score);
+	});
+
+	$(".secondGhost").click(function(){
+		var randomNumber = Math.floor(Math.random()*(12-1+1)+1);
+		var ghostTwoWeight = randomNumber;
+		score += ghostTwoWeight;
+		$("#theirWeight").text(score);
+	});
+
+	$(".thirdGhost").click(function(){
+		var randomNumber = Math.floor(Math.random()*(12-1+1)+1);
+		var ghostThreeWeight = randomNumber;
+		score += ghostThreeWeight;
+		$("#theirWeight").text(score);
+	});
+
+	$(".fourthGhost").click(function(){
+		var randomNumber = Math.floor(Math.random()*(12-1+1)+1);
+		var ghostFourWeight = randomNumber;
+		score += ghostFourWeight;
+		$("#theirWeight").text(score);
+	});
+
+	if (score === ghostWeight){
+			winning();
+		} else if (score > ghostWeight){
+			losing();
+	};
+
+});
+
+function winning(){
+	wins++;
+	$("#winsHere").text(wins);
+
+	randomGhostNumber = Math.floor(Math.random()*(120-1+19)+1);
+	ghostWeight = randomGhostNumber
+	$("#weight").text(ghostWeight);
+};
+
+function losing(){
+	losses++;
+	$("#lossesHere").text(losses);
+	randomGhostNumber = Math.floor(Math.random()*(120-1+19)+1);
+	ghostWeight = randomGhostNumber
 	$("#weight").text(ghostWeight);
 };
