ce528de7a1752ccab8ef5aa80f4d53dde0f020e0
James Frankwich <jamesfrankwich@Jamess-MacBook-Pro.local>
2017-12-03 02:50:50 -0500

    Some updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
+	// generate random image of your ghost
 	randomImg();
+	// generate random number for your ghost's weight
+	generateGhostWeight();
 });
 
 function randomImg(){
-var randomNumber = Math.floor(Math.random()*(4-1+1)+1);
-var imgName = "yourghost" + randomNumber + ".png";
-document.getElementById("imageid").src= "assets/images/" + imgName;
+	var randomNumber = Math.floor(Math.random()*(4-1+1)+1);
+	var imgName = "yourghost" + randomNumber + ".png";
+	document.getElementById("imageid").src= "assets/images/" + imgName;
+};
+
+function generateGhostWeight(){
+	var randomGhostNumber = Math.floor(Math.random()*(120-1+19)+1);
+	var ghostWeight = randomGhostNumber
+	$("#weight").text(ghostWeight);
 };
